1da2d9a440a52514aba459ed5e7f7c7ce8080075
Carbonell-Castillo <brucecastillo1204@gmail.com>
2024-05-30 22:47:26 -0600

    Implemented QR generator

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,26 @@
-
-document.getElementById('qrForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // Obtener el texto ingresado por el usuario
-    const textInput = document.getElementById('textInput').value;
-
-    // Crear el código QR usando la librería QRious
-    const qr = new QRious({
-        element: document.getElementById('qr-code'),
-        value: textInput,
-        size: 200 // Puedes ajustar el tamaño según tus necesidades
+document.getElementById('generateBtn').addEventListener('click', function() {
+    var qrInput = document.getElementById('qrInput').value;
+    var qrColor = document.getElementById('qrColor').value;
+    var qrAdditionalText= document.getElementById('qrText').value;
+    var qr = new QRious({
+        value: qrInput,
+        size: 250,
+        foreground: qrColor
     });
+
+    qr.image.onload = function() {
+        var canvas = document.getElementById('qrCanvas');
+        var ctx = canvas.getContext('2d');
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(qr.image, 0, 0);
+
+        if (qrAdditionalText) {
+            ctx.font = '16px Arial';
+            ctx.textAlign = 'center';
+            ctx.fillStyle = qrColor;
+            ctx.fillText(qrAdditionalText, canvas.width / 2, canvas.height - 20);
+        }
+    }
+
 });
